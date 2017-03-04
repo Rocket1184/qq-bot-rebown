4dff3cb9dc654e9671ca3b62c1b28b9dbf900ad2
rocka <i@rocka.me>
2017-03-04 20:42:45 +0800

    add ptqrlogin4 Referer, remove vfwebqq Origin

--- src/qq/url.js
+++ src/qq/url.js
@@ -12,9 +12,9 @@ module.exports = {
         return `https://ssl.ptlogin2.qq.com/ptqrlogin?ptqrtoken=${decoded}&webqq_type=10&remember_uin=1&login2qq=1&aid=501004106&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-123332&mibao_css=m_webqq&t=undefined&g=1&js_type=0&js_ver=10141&login_sig=&pt_randsalt=0`;
     },
     qrsigReferer: 'https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001',
+    ptlogin4Referer: 'http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1',
     getVfwebqqURL(ptwebqq) {
         return `http://s.web2.qq.com/api/getvfwebqq?ptwebqq=${ptwebqq}&clientid=53999199&psessionid=&t=${Math.random()}`;
     },
-    vfwebqqOrigin: 'http://s.web2.qq.com',
     vfwebqqReferer: 'http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1'
 };
