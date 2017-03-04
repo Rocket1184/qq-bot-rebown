f0c8066f560c0bb41db57bc55df88516e2ec5274
rocka <i@rocka.me>
2017-03-04 21:00:26 +0800

    add some comments

--- src/qq/index.js
+++ src/qq/index.js
@@ -38,23 +38,31 @@ class QQ {
     }
 
     async login() {
+        // Step0: prepare cookies, pgv_info and pgv_pvid
+        // http://pingjs.qq.com/tcss.ping.js  tcss.run & _cookie.init
         let initCookies = {
             pgv_info: `ssid=s${Codec.randPgv()}`,
             pgv_pvid: Codec.randPgv()
         };
         this.client.setCookie(initCookies);
         await this.client.get(URL.loginPrepare);
+
+        // Step1: download QRcode
         let qrCode = await this.client.get({ url: URL.qrcode, responseType: 'arraybuffer' });
         await writeFileAsync('/tmp/code.png', qrCode, 'binary');
         log.info('二维码下载完成，等待扫描...');
+        // open file, only for linux
         require('child_process').exec('xdg-open /tmp/code.png');
+
+        // Step2: 
         let scanSuccess = false;
         let quotRegxp = /'[^,]*'/g;
+        let ptqrloginURL = URL.getPtqrloginURL(this.client.getCookie('qrsig'));
         let ptlogin4URL;
         do {
             let responseBody = await this.client.get({
-                url: URL.getPtqrloginURL(this.client.getCookie('qrsig')),
-                headers: { Referer: URL.qrsigReferer },
+                url: ptqrloginURL,
+                headers: { Referer: URL.ptqrloginReferer },
             });
             log.debug(responseBody);
             let arr = responseBody.match(quotRegxp).map(i => i.substring(1, i.length - 1));
@@ -64,7 +72,11 @@ class QQ {
             }
         } while (!scanSuccess);
         log.info('二维码扫描完成');
+        // remove file, for linux(or macOS ?)
         require('child_process').exec('rm /tmp/code.png');
+
+        // Step3: find token 'vfwebqq' in cookie
+        // NOTICE: the request returns 302 when success. DO NOT REJECT 302.
         await this.client.get({
             url: ptlogin4URL,
             maxRedirects: 0,     // Axios follows redirect automatically, but we need to disable it here.
@@ -72,6 +84,8 @@ class QQ {
         });
         this.tokens.ptwebqq = this.client.getCookie('ptwebqq');
         log.info('获取 ptwebqq 成功');
+
+        // Step4: request token 'vfwebqq'
         let vfwebqqResp = await this.client.get({
             url: URL.getVfwebqqURL(this.tokens.ptwebqq),
             headers: { Referer: URL.vfwebqqReferer }
@@ -80,6 +94,8 @@ class QQ {
         this.tokens.vfwebqq = vfwebqqResp.result.vfwebqq;
         log.info('获取 vfwebqq 成功');
 
+        // Step5: psessionid and uin
+        // TODO: 
     }
 }
 
