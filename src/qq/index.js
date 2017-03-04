a782aef17c001091613e57f807b34c0a24237d10
rocka <i@rocka.me>
2017-03-04 11:06:18 +0800

    get ptwebqq works

--- src/qq/index.js
+++ src/qq/index.js
@@ -48,14 +48,35 @@ class QQ {
         await writeFileAsync('/tmp/code.png', qrCode, 'binary');
         log.info('二维码下载完成，等待扫描...');
         let scanSuccess = false;
+        let quotRegxp = /'[^,]*'/g;
+        let ptlogin4URL;
         do {
             let responseBody = await this.client.get({
                 url: URL.getPtqrloginURL(this.client.getCookie('qrsig')),
                 headers: { Referer: URL.qrsigReferer },
             });
             log.debug(responseBody);
-            let arr = responseBody.split(`','`);
-        } while (!scanSuccess)
+            let arr = responseBody.match(quotRegxp).map(i => i.substring(1, i.length - 1));
+            if (arr[0] === '0') {
+                scanSuccess = true;
+                ptlogin4URL = arr[2];
+            }
+        } while (!scanSuccess);
+        log.info('二维码扫描完成');
+        await this.client.get(ptlogin4URL);
+        this.tokens.ptwebqq = this.client.getCookie('ptwebqq');
+        log.info('获取 ptwebqq 成功');
+        let vfwebqqResp = await this.client.get({
+            url: URL.getVfwebqqURL(this.tokens.ptwebqq),
+            headers: {
+                Origin: URL.vfwebqqOrigin,
+                Referer: URL.vfwebqqReferer
+            }
+        });
+        log.debug(vfwebqqResp);
+        this.tokens.vfwebqq = vfwebqqResp.result.vfwebqq;
+        log.info('获取 vfwebqq 成功');
+
     }
 }
 
