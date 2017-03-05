23eb67b77fb4a9655cae909ba84556317209a6d6
rocka <i@rocka.me>
2017-03-05 15:31:17 +0800

    improve logging
    make 'log' method be global, so we could control log level by set env 'LOG_LEVEL'

--- src/qq/index.js
+++ src/qq/index.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const fs = require('fs');
-const Log = require('log');
 
 const URL = require('./url');
 const Codec = require('../codec');
 const Client = require('../httpclient');
-
-const log = new Log('debug');
 
 const AppConfig = {
     clientid: 53999199,
@@ -39,6 +36,8 @@ class QQ {
     }
 
     async login() {
+        log.info('(0/5) 开始登录，准备下载二维码');
+
         // Step0: prepare cookies, pgv_info and pgv_pvid
         // http://pingjs.qq.com/tcss.ping.js  tcss.run & _cookie.init
         let initCookies = {
@@ -51,7 +50,7 @@ class QQ {
         // Step1: download QRcode
         let qrCode = await this.client.get({ url: URL.qrcode, responseType: 'arraybuffer' });
         await writeFileAsync('/tmp/code.png', qrCode, 'binary');
-        log.info('二维码下载完成，等待扫描...');
+        log.info('(1/5) 二维码下载完成，等待扫描');
         // open file, only for linux
         require('child_process').exec('xdg-open /tmp/code.png');
 
@@ -72,7 +71,7 @@ class QQ {
                 ptlogin4URL = arr[2];
             }
         } while (!scanSuccess);
-        log.info('二维码扫描完成');
+        log.info('(2/5) 二维码扫描完成');
         // remove file, for linux(or macOS ?)
         require('child_process').exec('rm /tmp/code.png');
 
@@ -85,7 +84,7 @@ class QQ {
             headers: { Referer: URL.ptlogin4Referer }
         });
         this.tokens.ptwebqq = this.client.getCookie('ptwebqq');
-        log.info('获取 ptwebqq 成功');
+        log.info('(3/5) 获取 ptwebqq 成功');
 
         // Step4: request token 'vfwebqq'
         let vfwebqqResp = await this.client.get({
@@ -94,7 +93,7 @@ class QQ {
         });
         log.debug(vfwebqqResp);
         this.tokens.vfwebqq = vfwebqqResp.result.vfwebqq;
-        log.info('获取 vfwebqq 成功');
+        log.info('(4/5) 获取 vfwebqq 成功');
 
         // Step5: psessionid and uin
         let loginStat = await this.client.post({
@@ -113,7 +112,7 @@ class QQ {
         log.debug(loginStat);
         this.tokens.uin = loginStat.result.uin;
         this.tokens.psessionid = loginStat.result.psessionid;
-        log.info('获取 psessionid 和 uin 成功');
+        log.info('(5/5) 获取 psessionid 和 uin 成功');
     }
 }
 
