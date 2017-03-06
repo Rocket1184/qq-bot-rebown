e22c012bfaa0248e25f64c369013d87277a862f1
rocka <i@rocka.me>
2017-03-06 10:39:39 +0800

    replace constant query 't' to Math.random()

--- src/qq/url.js
+++ src/qq/url.js
@@ -22,17 +22,17 @@ module.exports = {
     referer151105: 'http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2',
     referer130916: 'http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1',
     recentList: 'http://d1.web2.qq.com/channel/get_recent_list2',
-    selfInfo: 'http://s.web2.qq.com/api/get_self_info2&t=0.1',
+    selfInfo: `http://s.web2.qq.com/api/get_self_info2&t=${Math.random()}`,
     poll: 'http://d1.web2.qq.com/channel/poll2',
     buddyMsg: 'http://d1.web2.qq.com/channel/send_buddy_msg2',
     groupMsg: 'http://d1.web2.qq.com/channel/send_qun_msg2',
     discuMsg: 'http://d1.web2.qq.com/channel/send_discu_msg2',
     getBuddy: 'http://s.web2.qq.com/api/get_user_friends2',
     onlineBuddies: (vfwebqq, psessionid) => `http://d1.web2.qq.com/channel/get_online_buddies2?vfwebqq=${vfwebqq}&clientid=53999199&psessionid=${psessionid}&t=${Math.random()}`,
-    buddyInfo: (uin, vfwebqq, psessionid) => `http://s.web2.qq.com/api/get_friend_info2?tuin=${uin}&vfwebqq=${vfwebqq}&clientid=53999199&psessionid=${psessionid}&t=0.1`,
-    buddyQQID: (uin, vfwebqq) => `http://s.web2.qq.com/api/get_friend_uin2?tuid=${uin}&type=1&vfwebqq=${vfwebqq}&t=0.1`,
+    buddyInfo: (uin, vfwebqq, psessionid) => `http://s.web2.qq.com/api/get_friend_info2?tuin=${uin}&vfwebqq=${vfwebqq}&clientid=53999199&psessionid=${psessionid}&t=${Math.random()}`,
+    buddyQQID: (uin, vfwebqq) => `http://s.web2.qq.com/api/get_friend_uin2?tuid=${uin}&type=1&vfwebqq=${vfwebqq}&t=${Math.random()}`,
     getDiscu: (vfwebqq, psessionid) => `http://s.web2.qq.com/api/get_discus_list?clientid=53999199&psessionid=${psessionid}&vfwebqq=${vfwebqq}&t=${Math.random()}`,
-    discuInfo: (discuss_id, psessionid, vfwebqq) => `http://d1.web2.qq.com/channel/get_discu_info?did=${discuss_id}&psessionid=${psessionid}&vfwebqq=${vfwebqq}&clientid=53999199&t=0.1`,
+    discuInfo: (discuss_id, psessionid, vfwebqq) => `http://d1.web2.qq.com/channel/get_discu_info?did=${discuss_id}&psessionid=${psessionid}&vfwebqq=${vfwebqq}&clientid=53999199&t=${Math.random()}`,
     getGroup: 'http://s.web2.qq.com/api/get_group_name_list_mask2',
-    groupInfo: (group_code, vfwebqq) => `http://s.web2.qq.com/api/get_group_info_ext2?gcode=${group_code}&vfwebqq=${vfwebqq}&t=0.1`
+    groupInfo: (group_code, vfwebqq) => `http://s.web2.qq.com/api/get_group_info_ext2?gcode=${group_code}&vfwebqq=${vfwebqq}&t=${Math.random()}`
 };
