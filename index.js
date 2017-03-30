34ac6b72e5a0048db0df42db8d9309456cd628dd
rocka <i@rocka.me>
2017-03-30 15:55:58 +0800

    add example to use MsgHandler

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const Log = require('log');
 global.log = new Log(process.env.LOG_LEVEL || 'info');
 
 const QQ = require('./src/qq');
+const MsgHandler = require('./src/qq/msg-handler');
 
-new QQ().run();
+const myHandler = new MsgHandler(
+    msg => {
+        console.log(`HandlerTest: ${JSON.stringify(msg, null, 4)}`);
+    },
+    'buddy', 'discu', 'group'
+);
+
+new QQ(myHandler).run();
