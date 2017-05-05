4537eae8dae4b50c0ee3a3c46a1b5cdc5601c33e
rocka <i@rocka.me>
2017-05-05 22:41:17 +0800

    remove Log() and modify handler example

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const Log = require('log');
-
-global.log = new Log(process.env.LOG_LEVEL || 'info');
-
 const QQ = require('./src/qq');
 const MsgHandler = require('./src/qq/msg-handler');
 
-const myHandler = new MsgHandler(
-    msg => {
-        console.log(`HandlerTest: ${JSON.stringify(msg, null, 4)}`);
+const buddyHandler = new MsgHandler(
+    (msg, qq) => {
+        qq.sendBuddyMsg(msg.id, `Hello ${msg.name}`);
     },
-    'buddy', 'discu', 'group'
+    'buddy'
 );
 
-new QQ(myHandler).run();
+const groupHandler = new MsgHandler(
+    msg => {
+        console.log(`HandlerTest: ${JSON.stringify(msg, null, 4)}`);
+    }, 'buddy', 'discu', 'group'
+);
+
+new QQ(buddyHandler, groupHandler).run();
