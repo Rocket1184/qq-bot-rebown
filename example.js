3177ab4ea4c9501de2bae07248674e98c90dfedb
rocka <i@rocka.me>
2017-09-23 20:23:33 +0800

    change example cookie path

--- example.js
+++ example.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const http = require('http');
 const { QQ } = require('.');
 
-const qq = new QQ({ cookiePath: '/tmp/qq-bot/cookie.txt' });
+const qq = new QQ({ cookiePath: '/tmp/my-qq-bot.cookie' });
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET' && req.url === '/code') {
