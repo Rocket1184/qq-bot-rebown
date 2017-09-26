c5652f872ba747f2c998bf70b51d4bba401881fa
rocka <i@rocka.me>
2017-09-26 20:11:47 +0800

    simplify example.js

--- example.js
+++ example.js
@@ -1,20 +1,11 @@
 'use strict';
 
-const fs = require('fs');
-const http = require('http');
 const { QQ } = require('.');
-
 const qq = new QQ({ cookiePath: '/tmp/my-qq-bot.cookie' });
-
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/code') {
-        fs.createReadStream(qq.options.qrcodePath).pipe(res);
-    }
-})
 
 qq.on('msg', (msg) => {
     console.log(JSON.stringify(msg));
-})
+});
 
 qq.on('buddy', (msg) => {
     qq.sendBuddyMsg(msg.id, `Hello, ${msg.name}`);
