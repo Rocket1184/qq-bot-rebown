3956a35f374a58be8e509bc556db4e4425d3dc27
rocka <i@rocka.me>
2018-01-18 23:04:11 +0800

    improve headless(id/pwd) login

--- src/qq/headless.js
+++ src/qq/headless.js
@@ -19,7 +19,7 @@ async function getTokens(u, p) {
     });
     const page = await browser.newPage();
     log.debug('Going to QZone login page...');
-    await page.goto('https://m.qzone.com/', { waitUntil: 'domcontentloaded' });
+    await page.goto('https://m.qzone.com', { waitUntil: 'domcontentloaded' });
     log.debug('Typing username and password...');
     await page.type('#u', `${u}`, { delay: 120 });
     await page.type('#p', `${p}`, { delay: 120 });
@@ -28,7 +28,20 @@ async function getTokens(u, p) {
     log.debug('Waiting for redirection...');
     await page.waitFor('#container');
     log.debug('Going to WebQQ login page...');
-    await page.goto('https://w.qq.com/');
+    await page.goto('https://w.qq.com', { waitUntil: 'networkidle2' });
+    log.debug('Waiting for avatar...');
+    await page.waitFor('iframe[name=ptlogin]');
+    const ptLoginFrame = page.frames().find(f => f.name() === 'ptlogin');
+    const ptLoginURL = ptLoginFrame.url()
+    log.debug(ptLoginURL);
+    if (ptLoginURL.startsWith('https://xui.ptlogin2.qq.com/cgi-bin/xlogin')) {
+        ptLoginFrame.waitFor('#qlogin_list a.face');
+        log.debug('Clicking avatar...');
+        const avatar = await ptLoginFrame.$('#qlogin_list a.face');
+        await avatar.click();
+    } else if (ptLoginURL.startsWith('https://w.qq.com/proxy.html')) {
+        log.debug('Nothing. Just wait for auto-login...');
+    }
     log.debug('Waiting for redirection...');
     await page.waitFor('#main_container');
     log.debug('Waiting for contacts to be loaded...');
