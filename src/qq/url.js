e302d5a76e1b9dce7aa37cc3dea1f1b9cdc3de75
rocka <i@rocka.me>
2018-01-19 02:21:10 +0800

    url: refine some referer url

--- src/qq/url.js
+++ src/qq/url.js
@@ -2,7 +2,7 @@
 
 const Codec = require('../codec');
 
-const bknHash = function(skey, init_str = 5381) {
+const bknHash = function (skey, init_str = 5381) {
     let hash_str = init_str;
     skey.split('').forEach(ch => {
         hash_str += (hash_str << 5) + ch.charCodeAt();
@@ -20,14 +20,10 @@ module.exports = {
         const decoded = Codec.decodeQrsig(qrsig);
         return `https://ssl.ptlogin2.qq.com/ptqrlogin?ptqrtoken=${decoded}&webqq_type=10&remember_uin=1&login2qq=1&aid=501004106&u1=http%3A%2F%2Fw.qq.com%2Fproxy.html%3Flogin2qq%3D1%26webqq_type%3D10&ptredirect=0&ptlang=2052&daid=164&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=0-0-123332&mibao_css=m_webqq&t=undefined&g=1&js_type=0&js_ver=10141&login_sig=&pt_randsalt=0`;
     },
-    ptqrloginReferer: 'https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001',
-    ptlogin4Referer: 'https://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1',
     getVfwebqqURL: ptwebqq => `https://s.web2.qq.com/api/getvfwebqq?ptwebqq=${ptwebqq}&clientid=53999199&psessionid=&t=${Date.now()}`,
-    vfwebqqReferer: 'https://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1',
     login2: 'https://d1.web2.qq.com/channel/login2',
-    login2Origin: 'https://d1.web2.qq.com',
-    login2Referer: 'https://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2',
-    msgOrigin: 'https://d1.web2.qq.com',
+    originD1: 'https://d1.web2.qq.com',
+    originS2: 'https://s.web2.qq.com',
     referer151105: 'https://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2',
     referer130916: 'https://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1',
     recentList: 'https://d1.web2.qq.com/channel/get_recent_list2',
@@ -46,5 +42,5 @@ module.exports = {
     groupInfo: (group_code, vfwebqq) => `https://s.web2.qq.com/api/get_group_info_ext2?gcode=${group_code}&vfwebqq=${vfwebqq}&t=${Date.now()}`,
     // 获取好友qq号码
     buddyGroupInfo: (skey) => `https://qun.qq.com/cgi-bin/qun_mgr/get_friend_list?bkn=${bknHash(skey)}`,
-    refererQun:`https://qun.qq.com/member.html`
+    refererQun: 'https://qun.qq.com/member.html'
 };
