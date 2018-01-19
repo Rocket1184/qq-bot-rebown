67e1bf7465bb1cfd2d5f00b4c9681db8024d21c7
rocka <i@rocka.me>
2018-01-19 12:38:52 +0800

    headless: do not close browser when debug

--- src/qq/headless.js
+++ src/qq/headless.js
@@ -3,13 +3,7 @@
 const Log = require('log');
 const log = global.log || new Log(process.env.LOG_LEVEL || 'info');
 
-const Utils = require('../utils');
 const Puppeteer = require('puppeteer');
-
-async function click(frame, selector) {
-    const elm = await frame.$(selector);
-    elm.click();
-}
 
 async function getTokens(u, p) {
     log.debug('Launching browser...');
@@ -45,15 +39,18 @@ async function getTokens(u, p) {
     log.debug('Waiting for redirection...');
     await page.waitFor('#main_container');
     log.debug('Waiting for contacts to be loaded...');
-    await page.waitFor('li.list_item');
+    await page.waitFor('li[id*=recent-item].list_item');
     log.debug('Getting tokens...');
     const vfwebqq = await page.evaluate('mq.vfwebqq');
     const ptwebqq = await page.evaluate('mq.ptwebqq');
     const psessionid = await page.evaluate('mq.psessionid');
     const cookies = await page.cookies('https://w.qq.com', 'https://web2.qq.com');
+    log.debug(`Cookie count: ${cookies.length}`);
+    const uin = cookies.find(ck => ck.name === 'uin').value;
     const cookieStr = cookies.reduce((str, ck) => `${str}${ck.name}=${ck.value}; `, '');
-    await browser.close();
+    if (process.env.HEADLESS_DEBUG !== 'true') await browser.close();
     const tokens = {
+        uin,
         vfwebqq,
         ptwebqq,
         psessionid,
